af9d692ce1a2458726c43e920501ceaba323e53e
Rongcui Dong <rongcuid@outlook.com>
2018-04-22 20:32:15 +0800

    Save

---
+++ src/CPUTop.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2018/04/22 20:01:00
+-- Design Name: 
+-- Module Name: CPUTop - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity CPUTop is
+--  Port ( );
+end CPUTop;
+
+architecture Behavioral of CPUTop is
+
+begin
+
+
+end Behavioral;
